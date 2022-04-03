d5285a5c5e39e302563a729a61beec720f44a2b2
Abdulhamid <73017095+Abdulhamid51@users.noreply.github.com>
2022-04-03 16:56:04 +0500

    coin_model_added

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Coin(models.Model):
+    name = models.CharField("Name", max_length=50)
+    symbol = models.CharField("Symbol", max_length=50)
+    price = models.FloatField("Price", default=0, blank=True)
+    rank = models.IntegerField("Rank", default=0, blank=True)
+    image = models.URLField("Image URL", max_length=1000, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['rank']
